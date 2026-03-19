dcf4fd7a3a827b003e8deddd3aec2799d1d46c8a
Raúl G. González <105838447+siakro@users.noreply.github.com>
2026-03-19 09:47:13 +0100

    [feat] implemented the StrEnum class (#2)

--- enum/enum.py
+++ enum/enum.py
@@ -1,19 +1,15 @@
 __all__ = [
-  "IntEnum",
   "enum",
+  "IntEnum",
+  "StrEnum",
 ]
 
 
-class IntEnum(int):
-  """An enumeration where all its values must be integers.
-
-  Observations:
-  - This must be defined with @enum too. Meta classes unsupported.
-  - This can be accessed from EnumType.LITERAL.
-  - @classmethod needed in __init_subclass__(), only in MicroPython.
+class _Enum:
+  """Base for the enums.
 
   Attributes:
-    _v2i: Value to enum instance map. Used by EnumType(value).
+    _v2i: Value to enum instance map. Used by OurEnumType(value).
   """
 
   @classmethod
@@ -21,41 +17,68 @@ class IntEnum(int):
     # (1) init
     cls._v2i = {}
 
-    # (2) adapt its enum instances created by MicroPython
-    # create the real enum instances
+    # (2) get the target data type such as int (for IntEnum) or str (for StrEnum)
+    Target = int if issubclass(cls, IntEnum) else str
+
+    # (3) create the real enum instances defined by the user
     for name, value in list(cls.__dict__.items()):
       # pre: skip private members, methods and callables
       if name.startswith("_") or callable(value):
         continue
 
+      # pre: value must be instance of Target
+      if not isinstance(value, Target):
+        raise TypeError(
+          f"Enum literal value '{value}' ({type(value).__name__}) must be {Target.__name__}."
+        )
+
       # create instance
       i = cls(value)
-      i._name_ = name
+      i._name_ = name  # type: ignore
 
       # add instance to the class
       setattr(cls, name, i)
       cls._v2i[value] = i
 
-    # (3) return enum class
+    # (4) return enum class
     return cls
+
+
+class IntEnum(int, _Enum):
+  """An enumeration where all its values must be integers.
+
+  Observations:
+  - This must be defined with @enum too. Meta classes unsupported.
+  - This can be accessed from something suc as, for example, OurEnumType.LITERAL.
+  - @classmethod needed in __init_subclass__(), due to MicroPython.
+  """
+
+
+class StrEnum(str, _Enum):
+  """An enumeration where all its values must be strings.
+
+  Observations:
+  - The IntEnum observations are applied to StrEnum.
+  """
 
 
 def enum(cls: type) -> type:
   """Generates the correct enumeration.
 
   Observations:
-  - Needed due to __init_subclass__() is not automatically called.
+  - Needed due to __init_subclass__() is not automatically called by MicroPython.
+  - It will be removed when MicroPython calls it.
   """
 
   # (1) adapt the literals
   cls.__init_subclass__()
 
-  # (2) replace __new__ for allowing EnumType(value)
-  def new(cls, v: int) -> IntEnum:
+  # (2) replace __new__ by factory function
+  def new(cls, v: int | str) -> IntEnum | StrEnum:
     if i := cls._v2i.get(v):
       return i
 
-    raise ValueError(f"Value '{v}' not found in {cls.__name__}(IntEnum).")
+    raise ValueError(f"Value '{v}' not found in {cls.__name__}.")
 
   setattr(cls, "__new__", new)
 
--- enum/enum_test.py
+++ enum/enum_test.py
@@ -1,37 +1,89 @@
 import unittest
 from enum import (
   IntEnum,
-  enum,  # pyright: ignore
+  StrEnum,  # type: ignore
+  enum,  # type: ignore
 )
 
 
 # The test int enum.
 @enum
-class Color(IntEnum):
+class IColor(IntEnum):
   RED = 1
-  BLUE = 2
-  GREEN = 3
+  GREEN = 2
+  BLUE = 3
+
+
+# The test str enum.
+@enum
+class SColor(StrEnum):
+  RED = "r"
+  GREEN = "g"
+  BLUE = "b"
 
 
 class TestIntEnum(unittest.TestCase):
-  def test_values(self):
+  def test_values(self) -> None:
     """Check that an IntEnum subclass is well-defined."""
 
-    assert Color.BLUE == 2
-    assert isinstance(Color.BLUE, int)
-    assert isinstance(Color.BLUE, Color)
+    assert IColor.BLUE == 3
+    assert isinstance(IColor.BLUE, int)
+    assert isinstance(IColor.BLUE, IColor)
 
-  def test_call_returns_i(self):
+  def test_call_returns_i(self) -> None:
     """Check that Color(value) returns a Color instance if value found."""
 
-    assert Color(1) is Color.RED
-    assert Color(2) is Color.BLUE
-    assert Color(3) is Color.GREEN
+    assert IColor(1) is IColor.RED
+    assert IColor(2) is IColor.GREEN
+    assert IColor(3) is IColor.BLUE
 
-  def test_call_raises_error(self):
+  def test_call_raises_error(self) -> None:
     """Check that Color(value) raises error if value not found."""
 
-    with self.assertRaises(ValueError) as cm:
-      Color(123)
+    # (1) act
+    with self.assertRaises(ValueError) as out:
+      IColor(123)
 
-    assert str(cm.exception) == "Value '123' not found in Color(IntEnum)."
+    # (2) assessment
+    assert str(out.exception) == "Value '123' not found in IColor."
+
+  def test_invalid_enum_value(self) -> None:
+    """Check that enum definition raises error if value is not int."""
+
+    # (1) act
+    with self.assertRaises(TypeError) as out:
+
+      @enum
+      class IColor(IntEnum):
+        RED = 1
+        GREEN = "2"  # type: ignore
+        BLUE = 3
+
+    # (2) assessment
+    assert str(out.exception) == "Enum literal value '2' (str) must be int."
+
+
+class TestStrEnum(unittest.TestCase):
+  def test_values(self) -> None:
+    """Check that a StrEnum subclass is well-defined."""
+
+    assert SColor.BLUE == "b"
+    assert isinstance(SColor.BLUE, str)
+    assert isinstance(SColor.BLUE, SColor)
+
+  def test_call_returns_i(self) -> None:
+    """Check that Color(value) returns a Color instance if value found."""
+
+    assert SColor("r") is SColor.RED
+    assert SColor("g") is SColor.GREEN
+    assert SColor("b") is SColor.BLUE
+
+  def test_call_raises_error(self) -> None:
+    """Check that Color(value) raises error if value not found."""
+
+    # (1) act
+    with self.assertRaises(ValueError) as out:
+      SColor("rgb")
+
+    # (2) assessment
+    assert str(out.exception) == "Value 'rgb' not found in SColor."
